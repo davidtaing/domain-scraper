fd91358a5bc96d8a36cdf71b0873247b04fcd584
David Taing <adavidtaing@gmail.com>
2022-02-02 18:19:27 +1100

    fix: listing schema definition

--- node/src/models/listing.model.js
+++ node/src/models/listing.model.js
@@ -1,6 +1,6 @@
 import { model, Schema } from "mongoose";
 
 // mixed "anything goes" Schema because
-const Listing = model("Listing", new Schema({ any: [] }));
+const Listing = model("Listing", new Schema({ any: Schema.Types.Mixed }));
 
 export default Listing;
