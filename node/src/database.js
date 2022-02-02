7444030432612585a16c6e35bd982e0c6dc62c6c
David Taing <adavidtaing@gmail.com>
2022-02-02 19:05:10 +1100

    feature: flush database before inserting documents

--- node/src/database.js
+++ node/src/database.js
@@ -8,8 +8,21 @@ const connectDb = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     }
-  ).then(() => console.log("Successfully connected to MongoDB"))
+  ).then(() => {
+    clearDatabase();
+    console.log("Successfully connected to MongoDB");
+  })
   .catch((err) => console.error(err));
+}
+
+
+async function clearDatabase() {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany({});
+  }
+  console.log("Flushed documents from database");
 }
 
 export default connectDb;
