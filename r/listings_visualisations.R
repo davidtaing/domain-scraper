e9c286ccef8b3ef81cc18a7e8bfbcc511263dd98
David Taing <adavidtaing@gmail.com>
2022-02-03 21:25:48 +1100

    R: add barchart

--- r/listings_visualisations.R
+++ r/listings_visualisations.R
@@ -8,6 +8,6 @@ m$find('{"propertyType": "ApartmentUnitFlat", "bedrooms": 1, "postcode": "2140"}
   select("displayableAddress", "suburb", "postcode", "displayPrice", "propertyType", "bedrooms", "bathrooms", "carspaces") %>%
     na.omit("displayPrice") -> results
 
-median <- median(results$displayPrice)
+p <- ggplot(results, aes(displayPrice)) + geom_bar(fill = "skyblue4");
 
-median
+p
