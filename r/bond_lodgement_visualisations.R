06fad8405db9f73e4abf2a16846cf79a45e35336
David Taing <adavidtaing@gmail.com>
2022-02-16 19:32:49 +1100

    implement: bond lodgement plot generation

---
+++ r/bond_lodgement_visualisations.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+
+bond_lodgements_path <- "../data/Rental-bond-lodgements-data-January-2022.xlsx"
+bond_period <- "January-2022"
+postcodes <- c("2137", "2138", "2140")
+dwelling_types <- c("F", "H", "T", "O", "U");
+
+bond_data <- readxl::read_excel(bond_lodgements_path)
+
+for (postcode in postcodes) {
+  for (dwelling_type in dwelling_types) {
+    bond_data %>% filter(Postcode == postcode) %>% filter(Type == dwelling_type) -> bonds_by_type
+    bedrooms <- distinct(as.data.frame(bonds_by_type$Bedrooms));
+    
+    print(postcode)
+    print(dwelling_type)
+    print(bedrooms)
+    
+    for (bedroom_amount in 1:nrow(bedrooms)) {
+      print(bedroom_amount)
+      
+      bonds_by_type %>% filter(Bedrooms == bedroom_amount) -> bonds_by_type_and_bedrooms    
+      print(bonds_by_type_and_bedrooms)
+      
+      if (nrow(bonds_by_type_and_bedrooms) > 0) {
+        print(str_c("Processing ", nrow(bonds_by_type_and_bedrooms), " results"))
+        
+        # Create Title for Plot & File Path
+        title <- str_c(postcode, "_" , dwelling_type, "_", bedroom_amount, "BR_", bond_period)
+        path <- str_c(title, ".png")
+        subtitle <- str_c("(N = ", count(bonds_by_type_and_bedrooms), ")")
+        
+        print(title);
+        print(path);
+        
+        p <- ggplot(bonds_by_type_and_bedrooms, aes(Rent)) + 
+          geom_bar(fill = "skyblue4") + 
+          labs(x = "Rent Per Week", y = "Property Count", title = title, subtitle = subtitle) +
+          theme(axis.text.x = element_text(angle=90));
+        p
+        
+        ggsave(path="../plots/bond_lodgements", filename=path, plot=last_plot(), device = "png")
+      }
+    }
+  }
+}
