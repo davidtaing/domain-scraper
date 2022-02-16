a06da7a6340dbc7c4594fa2bb204207872ac8296
David Taing <adavidtaing@gmail.com>
2022-02-16 12:13:46 +1100

    implement: batch bar chart generation

--- r/listings_visualisations.R
+++ r/listings_visualisations.R
@@ -1,13 +1,50 @@
 library(mongolite)
-library(dplyr)
-library(ggplot2)
+library(tidyverse)
+library(jsonlite)
 
-m <- mongo(db = "domain", collection ="listings")
+# Retrieve Property Types
+propType_path <- "./propertyFilters.json";
+propertyCategories <- read_json(propType_path, simplifyDataFrame=TRUE);
+postcodes <- c("2137", "2138", "2140");
 
-m$find('{"propertyType": "ApartmentUnitFlat", "bedrooms": 1, "postcode": "2140"}') %>% 
-  select("displayableAddress", "suburb", "postcode", "displayPrice", "propertyType", "bedrooms", "bathrooms", "carspaces") %>%
-    na.omit("displayPrice") -> results
+# Create Filter
+setClass("filter", slots=list(postcode="character", propertyType="character", bedrooms="numeric"))
 
-p <- ggplot(results, aes(displayPrice)) + geom_bar(fill = "skyblue4");
+# Connect to MongoDB
+m <- mongo(db = "domain", collection ="listings");
 
-p
+processListings <- function(postcode, propertyType, bedrooms) {
+  filter <- data.frame(postcode = postcode, propertyType = propertyType, bedrooms=bedrooms);
+  mongoose_query_string <- toJSON(unbox(filter), pretty=TRUE, raw="mongo");
+  print(mongoose_query_string);
+  # Query DB
+  #m$find(mongoose_query_string) %>%
+  #    na.omit("displayPrice") -> results
+  results <- m$find(mongoose_query_string)
+  
+  if (nrow(results) > 0) {
+    print(str_c("Processing ", nrow(results), " results"))
+    # Create Title for Plot & File Path
+    title <- str_c(postcode, "_" , propertyType, "_", bedrooms ,"BR_", Sys.Date())
+    path <- str_c(title, ".png")
+    subtitle <- str_c("(N = ", count(results), ")")
+    print(path)
+    print(path)
+    
+    p <- ggplot(results, aes(displayPrice)) + geom_bar(fill = "skyblue4") + labs(x = "Rent Per Week", y = "Property Count", title = title, subtitle = subtitle);
+    p
+    
+    ggsave(path="visualisations", filename=path, plot=last_plot())
+  } else {
+    print("No results found.")
+  }
+}
+
+for (postcode in postcodes) {
+  for (row in 1:nrow(propertyCategories)) {
+    propertyType <- propertyCategories[row, "propertyType"]
+    bedrooms <-propertyCategories[row, "bedrooms"]
+    processListings(postcode, propertyType, bedrooms)
+  }
+} 
+  
